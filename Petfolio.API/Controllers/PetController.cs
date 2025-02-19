089fd92fe13de8073bc9937f86d7fc4df0beb138
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-02-19 02:42:39 -0300

    GetAllPets

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
@@ -31,6 +32,23 @@ public class PetController : ControllerBase
         var useCase = new UpdatePetUseCase();
 
         useCase.Execute(id, request);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllPetJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllPetsUseCase();
+
+        var response = useCase.Execute();
+
+        if(response.Pets.Any())
+        {
+            return Ok(response);
+        }
 
         return NoContent();
     }
